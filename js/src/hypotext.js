505d36bc750e6eb085c50285f74296aaf0765830
Michael Liu <mikeliu@evidentpoint.com>
2020-03-04 11:04:48 -0800

    refactor(js): now you're parsing with context

--- js/src/hypotext.js
+++ js/src/hypotext.js
@@ -5,31 +5,31 @@ module.exports = parentNode => {
     }
 
     // Start the Text Entries Array
-    const textEntries = [""];
+    const entityList = [];
 
     // Recursively process Nodes
-    processNode(parentNode, textEntries);
+    processNode(parentNode, entityList);
 
     // Finally Join the entries
-    return joinEntries(textEntries);
+    return joinEntries(entityList);
 };
 
 // Char codes for \t, \n, and non-&nbsp; space character
 const whitespaces = [9, 10, 32];
-const isWhitespace = charCode => {
+const isCharWhitespace = charCode => {
     return whitespaces.includes(charCode);
 };
 const trimBeginAndEnd = string => {
     // Get the first and last non-whitespace character index
     let firstNonWhite, lastNonWhite;
     for (let index = 0; index < string.length; index++) {
-        if (!isWhitespace(string.charCodeAt(index))) {
+        if (!isCharWhitespace(string.charCodeAt(index))) {
             firstNonWhite = index;
             break;
         }
     }
     for (let index = string.length - 1; index >= 0; index--) {
-        if (!isWhitespace(string.charCodeAt(index))) {
+        if (!isCharWhitespace(string.charCodeAt(index))) {
             lastNonWhite = index;
             break;
         }
@@ -46,52 +46,117 @@ const trimBeginAndEnd = string => {
         lastNonWhite ? lastNonWhite + 1 : undefined
     );
 };
-const joinEntries = entries => {
-    const processed = entries.map(element => {
-        // Nulls come from <p> and <br> tags, and denotes a definite new line
-        // The null value will get turned into string if there is any content
-        //  added which is not in a sub-container
-        if (element === null) {
-            return "";
+const collapseWhitespace = string => {
+    // Collapse all other sequential whitespace into a single whitespace
+    const textElements = [];
+    let startOfNonWhite = null;
+    for (let index = 0; index < string.length; index++) {
+        if (
+            startOfNonWhite === null &&
+            !isCharWhitespace(string.charCodeAt(index))
+        ) {
+            startOfNonWhite = index;
+            continue;
         }
+        if (
+            startOfNonWhite !== null &&
+            isCharWhitespace(string.charCodeAt(index))
+        ) {
+            textElements.push(string.slice(startOfNonWhite, index));
+            startOfNonWhite = null;
+            continue;
+        }
+    }
+    if (startOfNonWhite !== null) {
+        textElements.push(string.slice(startOfNonWhite));
+    }
 
-        // Trim
-        const sliced = trimBeginAndEnd(element);
-        if (!sliced) {
-            // After trimming, if the string is empty
-            // Return Null, which will get filtered out later
-            return null;
-        }
+    return textElements.join(" ");
+};
+const joinEntries = entitiesList => {
+    const processed = [];
 
-        // Collapse all other sequential whitespace into a single whitespace
-        const textElements = [];
-        let startOfNonWhite = null;
-        for (let index = 0; index < sliced.length; index++) {
-            if (
-                startOfNonWhite === null &&
-                !isWhitespace(sliced.charCodeAt(index))
-            ) {
-                startOfNonWhite = index;
-                continue;
-            }
-            if (
-                startOfNonWhite !== null &&
-                isWhitespace(sliced.charCodeAt(index))
-            ) {
-                textElements.push(sliced.slice(startOfNonWhite, index));
-                startOfNonWhite = null;
-                continue;
-            }
-        }
-        if (startOfNonWhite !== null) {
-            textElements.push(sliced.slice(startOfNonWhite));
-        }
+    for (let i = 0; i < entitiesList.length; i++) {
+        const entity = entitiesList[i];
 
-        return textElements.join(" ");
-    });
+        // console.log('Entity: ', entity)
+
+        // TEXT Node
+        switch (entity.type) {
+            case entityType.TEXT:
+                // If preformatted, simply push it
+                if (entity.condition === "pre") {
+                    processed.push(`\n${entity.content}\n`);
+                    break;
+                }
+
+                const normalized = entity.content.normalize()
+
+                // Trim
+                const trimmed = trimBeginAndEnd(normalized);
+                if (!trimmed) {
+                    processed.push(null);
+                    break;
+                }
+
+                // Otherwise, collapse all whitespace
+                processed.push(collapseWhitespace(trimmed));
+
+                break;
+
+            case entityType.BREAK:
+                processed.push("\n");
+                break;
+
+            case entityType.PARAGRAPH:
+                // <p> always generates newlines, others do not
+                if (entity.tag === "p") {
+                    processed.push("\n");
+                } else {
+                    if(processed.length === 0){
+                        processed.push(null)
+                        break;
+                    }
+
+                    let addedToList = false
+                    // Iterate backwards, and if we see another PARAGRAPH directly before,
+                    //  don't add new line
+                    for (
+                        let previousIndex = processed.length - 1;
+                        previousIndex >= 0;
+                        previousIndex--
+                    ) {
+                        // console.log("Processed: ", processed[previousIndex]);
+                        // console.log(
+                        //     "EntitesList: ",
+                        //     entitiesList[previousIndex]
+                        // );
+                        if (processed[previousIndex] != null) {
+                            if (
+                                entitiesList[previousIndex].type !==
+                                entityType.PARAGRAPH
+                            ) {
+                                processed.push("\n");
+                            } else {
+                                processed.push(null)
+                            }
+                            addedToList = true
+                            break;
+                        }
+                    }
+                    if(!addedToList){
+                        processed.push(null)
+                    }
+                }
+                break;
+        }
+    }
+
+    // console.log(entitiesList);
+    // console.log(processed);
 
     // Filter out all Nulls
-    const filtered = processed.filter(element => element !== null).join("\n");
+    const filtered = processed.filter(element => element !== null).join("");
 
     // Special case for if all elements were empty
     if (filtered.length === 0) {
@@ -103,12 +168,12 @@ const joinEntries = entries => {
 };
 
 const blacklist = ["script", "style", "head"];
-// const textEntryType = {
-//     BREAK: "break",
-//     TEXT: "text",
-//     PARAGRAPH: "paragraph"
-// };
-const processNode = (node, textEntries) => {
+const entityType = {
+    TEXT: "text",
+    BREAK: "break",
+    PARAGRAPH: "paragraph"
+};
+const processNode = (node, entityList) => {
     const tag = node.tagName && node.tagName.toLowerCase();
 
     // Filter blacklist
@@ -118,23 +183,32 @@ const processNode = (node, textEntries) => {
 
     // If this node has children
     if (node.hasChildNodes()) {
-        checkForParagraph(tag, textEntries, false);
+        // Process Preformatted Tags
+        if (tag === "pre") {
+            entityList.push({
+                type: entityType.TEXT,
+                content: node.textContent,
+                condition: "pre"
+            });
+            return;
+        }
+
+        checkForBlockConstructs(tag, entityList);
 
         // Iterate
         node.childNodes.forEach(node => {
             // Process nodes recursively
-            processNode(node, textEntries);
+            processNode(node, entityList);
         });
 
-        checkForParagraph(tag, textEntries, true);
+        checkForBlockConstructs(tag, entityList);
         return;
     }
 
     // Process the node if it is a Leaf Node
-    // TODO: Process Preformatted Tags
 
     if (node.nodeType === Node.TEXT_NODE) {
-        addEntryToArray(node.textContent, textEntries);
+        addTextToEntities(node.textContent, entityList);
         return;
     }
 
@@ -142,43 +216,38 @@ const processNode = (node, textEntries) => {
 
     switch (tag) {
         case "br":
-            textEntries.push(null);
+            entityList.push({ type: entityType.BREAK });
             break;
         case "img":
             // Count |alt| attribute of image tags as text
             if (node.hasAttribute("alt")) {
-                addEntryToArray(` ${node.getAttribute("alt")}`, textEntries);
+                addTextToEntities(` ${node.getAttribute("alt")}`, entityList);
             }
             break;
     }
 };
 
-const addEntryToArray = (entry, array) => {
+const addTextToEntities = (text, entityList) => {
     // If the last element was null, it came from a break (br, p, div, etc.)
-    if (array[array.length - 1] === null) {
-        array[array.length - 1] = "";
+    if (
+        entityList.length > 0 &&
+        entityList[entityList.length - 1].type === entityType.TEXT
+    ) {
+        entityList[entityList.length - 1].content += text;
+    } else {
+        entityList.push({ type: entityType.TEXT, content: text });
     }
-
-    array[array.length - 1] += entry;
 };
 
-const paragraphConstructs = ["div", "p", "section"];
-const checkForParagraph = (tag, textEntries, closing) => {
+const blockConstructs = ["div", "p", "section"];
+const checkForBlockConstructs = (tag, textEntries) => {
     // If this construct is a Run of Phrasing
 
     // P and Div have different behaviours
     // Specifically P will leave consecutive \n characters, whereas Div does not
     // Refer to Container and Paragraphs tests respectively
 
-    if (!tag) {
-        return;
-    }
-
-    if (closing && tag === "p") {
-        textEntries.push(null);
-    }
-
-    if (paragraphConstructs.includes(tag)) {
-        textEntries.push("");
+    if (blockConstructs.includes(tag)) {
+        textEntries.push({ type: entityType.PARAGRAPH, tag: tag });
     }
 };
