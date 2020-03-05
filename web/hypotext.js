6141f1b75a880d9af0c5e193bf29fa642787b863
Juan Carlos Corona Romero <juanc@evidentpoint.com>
2020-03-04 19:34:51 -0800

    feat(web): improve reference

--- web/hypotext.js
+++ web/hypotext.js
@@ -2,10 +2,18 @@ globalThis.hypotext = input => {
   var div = document.createElement("div");
   div.innerHTML = input;
   document.body.appendChild(div);
-  Array.from(div.querySelectorAll("a, img")).forEach(el => {
+  Array.from(div.querySelectorAll("a, img, area, input")).forEach(el => {
     if (el.childNodes.length === 0) {
       el.outerHTML = el.alt || "";
     }
+  });
+  Array.from(div.querySelectorAll("wbr")).forEach(el => {
+    if (el.childNodes.length === 0) {
+      el.outerHTML = "&#8203;";
+    }
+  });
+  Array.from(div.querySelectorAll("svg")).forEach(el => {
+    el.outerHTML = el.innerHTML;
   });
   const text = div.innerText;
   div.remove();
