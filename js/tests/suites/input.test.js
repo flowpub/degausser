f92a43e87f0fd239364f08c910633d0c80ec10dc
Michael Liu <mikeliu@evidentpoint.com>
2020-03-10 15:29:01 -0700

    test(js): now tests more nodes

--- js/tests/suites/input.test.js
+++ js/tests/suites/input.test.js
@@ -9,15 +9,36 @@ describe("Testing input node types", () => {
 
     test("DocumentFragment Node", () => {
         const fragment = document.createDocumentFragment();
-        
+
         let element = document.createElement("p");
-        element.innerHTML = 'Ho!'
-        fragment.appendChild( element );
+        element.innerHTML = "Ho!";
+        fragment.appendChild(element);
 
         element = document.createElement("p");
-        element.innerHTML = 'Ho!'
-        fragment.appendChild( element );
+        element.innerHTML = "Ho!";
+        fragment.appendChild(element);
 
         expect(degausser(fragment)).toBe("Ho!\n\nHo!");
+    });
+
+    test("Element Node", () => {
+        const element = document.createElement("p");
+        element.innerHTML = "Hey!";
+
+        expect(degausser(element)).toBe("Hey!");
+    });
+
+    test("Text Node", () => {
+        const text = document.createTextNode("Yo");
+
+        expect(degausser(text)).toBe("Yo");
+    });
+
+    test("Comment Node", () => {
+
+        const comment = document.createComment("This is a comment");
+
+        expect(degausser(comment)).toBe("");
+
     });
 });
