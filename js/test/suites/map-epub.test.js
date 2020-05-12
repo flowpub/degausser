04df514543100f1cb84183552517aabfd71f330d
Michael Liu <mikeliu@evidentpoint.com>
2020-05-12 13:39:18 -0700

    test(js): tests for mapping function

---
+++ js/test/suites/map-epub.test.js
@@ -0,0 +1,48 @@
+import { readdirSync, readFileSync, writeFileSync } from 'fs'
+import glob from 'glob'
+import { join, basename, dirname, extname } from 'path'
+
+import { degausser, mapWithDegausser } from '../../src/degausser'
+
+const pathToEpubs = '../testdata/parsed_epubs'
+const epubs = readdirSync(pathToEpubs).map(epub => join(pathToEpubs, epub))
+
+epubs.forEach(epubDir => {
+  test(`Testing Map on EPUB: ${basename(epubDir)}`, () => {
+    glob(`${epubDir}/**/*.{html,xhtml,htm}`, (_, files) => {
+      for (const epubFile of files) {
+        const sourceHTML = readFileSync(epubFile, 'utf8')
+
+        const parser = new DOMParser()
+        const encoding =
+          extname(epubFile) === 'xhtml' ? 'application/xhtml+xml' : 'text/html'
+        let doc
+        try {
+          doc = parser.parseFromString(sourceHTML, encoding)
+        } catch (error) {
+          console.error(error)
+        }
+        expect(doc).toBeTruthy()
+
+
+        if (doc) {
+          const root = doc.documentElement
+          expect(root).toBeTruthy()
+
+          const output = degausser(doc.documentElement)
+          expect(output).toBeTruthy()
+          
+          const map = mapWithDegausser(doc.documentElement)
+
+          for (const mapSection of map) {
+            const sliced = output.slice(
+              mapSection.start,
+              mapSection.start + mapSection.length,
+            )
+            sliced === mapSection.content
+          }
+        }
+      }
+    })
+  })
+})
---
+++ js/test/suites/map.test.js
@@ -0,0 +1,212 @@
+import { degausser, mapWithDegausser } from '../../src/degausser'
+import { getInputFile } from '../util'
+
+describe(`Testing Map on Containers`, () => {
+  const file = getInputFile('containers.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Crazy`, () => {
+  const file = getInputFile('crazy.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on images`, () => {
+  const file = getInputFile('images.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on links`, () => {
+  const file = getInputFile('links.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Lists`, () => {
+  const file = getInputFile('lists.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Paragraphs`, () => {
+  const file = getInputFile('paragraphs.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Scripts`, () => {
+  const file = getInputFile('scripts.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Spans`, () => {
+  const file = getInputFile('spans.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Tables`, () => {
+  const file = getInputFile('tables.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
+
+describe(`Testing Map on Whitespaces`, () => {
+  const file = getInputFile('whitespaces.json')
+
+  file.forEach((element, index) => {
+    test(`Testing ${index}`, () => {
+      document.documentElement.innerHTML = element.i
+
+      const map = mapWithDegausser(document.documentElement)
+      const string = degausser(document.documentElement)
+
+      for (const mapSection of map) {
+        const sliced = string.slice(
+          mapSection.start,
+          mapSection.start + mapSection.length,
+        )
+        sliced === mapSection.content
+      }
+    })
+  })
+})
