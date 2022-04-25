fefeebe1a0faafeda7ef26b41befda35a3444719
Sanjaya Gebrial <sanjayag@evidentpoint.com>
2022-04-25 16:48:08 -0700

    fix(web): fix broken tests

--- web/degausser.js
+++ web/degausser.js
@@ -2,9 +2,14 @@ globalThis.degausser = input => {
   var div = document.createElement("div");
   div.innerHTML = input;
   document.body.appendChild(div);
-  Array.from(div.querySelectorAll("a, img, area, input")).forEach(el => {
+  Array.from(div.querySelectorAll("a, input")).forEach(el => {
     if (el.childNodes.length === 0) {
       el.outerHTML = el.alt || "";
+    }
+  });
+  Array.from(div.querySelectorAll("img, area, image")).forEach(el => {
+    if (el.childNodes.length === 0) {
+      el.outerHTML = el.alt || String.fromCharCode(31).repeat(100);
     }
   });
   Array.from(div.querySelectorAll("wbr")).forEach(el => {
